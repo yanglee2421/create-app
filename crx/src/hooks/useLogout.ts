45b99c569816f147b53cc2eef9df0b4f94f68cb5
ZhangSheng <yanglee2421@gmail.com>
2023-08-18 17:08:12 +0800

    Fix useLogout

--- crx/src/hooks/useLogout.ts
+++ crx/src/hooks/useLogout.ts
@@ -1,29 +1,12 @@
-// API Hooks
-import { useMutation } from "@tanstack/react-query";
-
-// Redux Hooks
-import { useAppDispatch, sliceLogin } from "@/redux";
-
-// Toast Imports
-import { toast } from "react-hot-toast";
+// Redux Imports
+import { useAppDispatch, useAppSelector, mutateLogin } from "@/redux";
 
 export function useLogout() {
-  // Redux Hooks
   const dispatch = useAppDispatch();
+  const isLoading = useAppSelector((s) => s.login.isLoading);
+  const mutate = () => {
+    dispatch(mutateLogin({ isLogged: false }));
+  };
 
-  // API Hooks
-  return useMutation<unknown, Error>({
-    async mutationFn() {
-      await chrome.storage.sync.set({ isLogged: false, usr: null });
-      const action = sliceLogin.actions.islogged(false);
-      dispatch(action);
-      return await chrome.storage.sync.get(["isLogged", "usr"]);
-    },
-    onError(err) {
-      toast.error(err.message);
-    },
-    onSuccess() {
-      toast.success("Logout Successlly!");
-    },
-  });
+  return { mutate, isLoading };
 }
