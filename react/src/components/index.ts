e275582d3e26e6e74114404c3536130f52c7ff71
Yang_Lee <yanglee2421@outlook.com>
2023-08-21 01:15:40 +0800

    sort out code

--- react/src/components/index.ts
+++ react/src/components/index.ts
@@ -1,11 +1,9 @@
 export { TinyMCE } from "./tiny-mce";
-export { Reverse } from "./reverse";
 
 // ** HOC
 export { withPortal } from "./with-portal";
 
 export { Counter } from "./Counter";
-export { Collapse } from "./collapse";
 
 // ** Form
 export * from "./item-radio";
