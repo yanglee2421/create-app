70ff7164607a06f74ab4bde0b3ce3f44768a8c06
Yang_Lee <yanglee2421@outlook.com>
2023-08-26 05:39:26 +0800

    vite-legacy

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,6 +1,7 @@
 // Vite Imports
 import { ConfigEnv, defineConfig, UserConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import legacy from "@vitejs/plugin-legacy";
 
 // NodeJs Imports
 import { resolve } from "node:path";
@@ -10,7 +11,7 @@ export default defineConfig((configEnv) => {
   void configEnv;
 
   return {
-    plugins: [react()],
+    plugins: [react(), legacy()],
 
     resolve: {
       alias: {
